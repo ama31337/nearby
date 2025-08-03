74362bd0dd413bb5d919fb590a739bfc8ad52fc9
ama31337 <ama31337@protonmail.com>
2025-08-03 12:55:57 +0700

    commit

---
+++ env.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Bot config
+BotAPIKey = "123" # API Keythat you get from @BotFather
+AdminChatID = 123
+
+POOL_NAME = "lux.poolv1.near"
+NEAR_NETWORK = "mainnet"
+
+MAX_MISSED_BLOCKS = 1
+MAX_MISSED_CHUNKS = 1
+MAX_MISSED_ENDORSEMENTS = 1
+
+MIN_BLOCK_PERCENT = 90
+MIN_CHUNK_PERCENT = 90
+MIN_ENDORSE_PERCENT = 90
